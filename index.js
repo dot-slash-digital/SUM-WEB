433f36ed4c14df7c56d8d915a3f3fd2022c8260c
Matthew Bierman <matthew.bierman2@gmail.com>
2020-02-07 20:20:02 -0600

    Contact form input validation

--- index.js
+++ index.js
@@ -1,11 +1,89 @@
+// Enable scrollspy
+$('body').scrollspy({target: "#navbar", offset: 300});
+
 // Smooth scrolling for navbar links
 $("nav a").on('click', function(e) {
     e.preventDefault();
     var hash = this.hash;
+    console.log($("#navbar").height(), hash);
 
     $('html, body').animate({
-        scrollTop: ($(hash).offset().top)
+        scrollTop: ($(hash).offset().top - $("#navbar").height())
     }, 800, function() {
         window.location.hash = hash;
     });
+});
+
+// Submission and input validation
+$("form").submit(function() {
+    // Full Name (required)
+    if ($("#contact-full-name").val() == "") {
+        $("#contact-full-name").removeClass("is-valid");
+        $("#contact-full-name").addClass("is-invalid");
+    } else {
+        $("#contact-full-name").removeClass("is-invalid");
+        $("#contact-full-name").addClass("is-valid");
+    }
+    
+    // Phone Number (required)
+    if ($("#contact-phone-number").val().replace(/\D/g,'').length != 10) {
+        $("#contact-phone-number").removeClass("is-valid");
+        $("#contact-phone-number").addClass("is-invalid");
+    } else {
+        $("#contact-phone-number").removeClass("is-invalid");
+        $("#contact-phone-number").addClass("is-valid");
+    }
+    
+    // Email Address (required)
+    if ($("#contact-email-address").val() == "") {
+        $("#contact-email-address").removeClass("is-valid");
+        $("#contact-email-address").addClass("is-invalid");
+    } else {
+        $("#contact-email-address").removeClass("is-invalid");
+        $("#contact-email-address").addClass("is-valid");
+    }
+    
+    // Project Description (required)
+    if ($("#contact-project-description").val() == "") {
+        $("#contact-project-description").removeClass("is-valid");
+        $("#contact-project-description").addClass("is-invalid");
+    } else {
+        $("#contact-project-description").removeClass("is-invalid");
+        $("#contact-project-description").addClass("is-valid");
+    }
+    
+    if ($("#contact-full-name").val() != "" &&
+        $("#contact-phone-number").val().replace(/\D/g,'').length == 10 &&
+        $("#contact-email-address").val() != "" &&
+        $("#contact-project-description").val() != "")
+        return true;
+    else
+        return false;
+});
+
+// Remove form validity classes if a user edits the input
+$("input").on('input', function() {
+    $(this).removeClass("is-valid");
+    $(this).removeClass("is-invalid");
+});
+$("textarea").on('input', function() {
+    $(this).removeClass("is-valid");
+    $(this).removeClass("is-invalid");
+});
+
+// Set submit button to disabled on page load
+$(document).ready(function() {
+    $("#contact-submit").addClass("disabled");
+});
+
+// Enable submit button when all required fields are valid
+$(".form-control").keyup(function() {
+    // When all required fields are valid
+    if ($("#contact-full-name").val() != "" &&
+        $("#contact-phone-number").val().replace(/\D/g,'').length == 10 &&
+        $("#contact-email-address").val() != "" &&
+        $("#contact-project-description").val() != "")
+        $("#contact-submit").removeClass("disabled");
+    else
+        $("#contact-submit").addClass("disabled");
 });
